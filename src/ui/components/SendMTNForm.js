293a5a3035d32df8f2c8f2cc0d150d18714825ba
Pablo Enrici <pablo@bloq.com>
2018-02-01 16:44:47 -0300

    Add refactor and styling for form components

--- src/ui/components/SendMTNForm.js
+++ src/ui/components/SendMTNForm.js
@@ -1,93 +1,159 @@
 // import PropTypes from 'prop-types';
-import { Btn, Sp } from '../common';
+import { BaseBtn, TextInput, TxIcon, Flex, Btn, Sp } from '../common';
 import styled from 'styled-components';
 import React from 'react';
+import Web3 from 'web3';
 
-const Container = styled.form`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-`;
-
-const Field = styled.div``;
-
-const Row = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 2.4rem;
-`;
-
-const Label = styled.label`
-  line-height: 1.6rem;
-  font-size: 1.3rem;
+const MaxBtn = BaseBtn.extend`
+  float: right;
+  line-height: 1.8rem;
+  opacity: 0.5;
+  font-size: 1.4rem;
   font-weight: 600;
-  letter-spacing: 0.5px;
+  letter-spacing: 1.4px;
   text-shadow: 0 1px 1px ${p => p.theme.colors.darkShade};
-`;
+  margin-top: 0.4rem;
 
-const Input = styled.input`
-  border: none;
-  display: block;
-  height: 5.6rem;
-  padding: 0.8rem 1.6rem;
-  background-color: rgba(126, 97, 248, 0.2);
-  margin-top: 0.8rem;
-  width: 100%;
-  line-height: 4rem;
-  color: rgba(255, 255, 255, 0.5);
-  font-size: 1.3rem;
-  font-weight: 600;
-  letter-spacing: 0.5px;
-  text-shadow: 0 1px 1px ${p => p.theme.colors.darkShade};
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const BtnContainer = styled.div`
   background-image: linear-gradient(to bottom, #272727, #323232);
-  height: 100%;
   padding: 6.4rem 2.4rem;
   flex-grow: 1;
+  height: 100%;
 `;
 
 export default class SendMTNForm extends React.Component {
   // static propTypes = {};
 
+  state = {
+    status: 'init',
+    errors: {},
+    toAddress: null,
+    ethAmount: null,
+    usdAmount: null,
+    ethFee: null,
+    usdFee: null
+  };
+
+  onMaxClick = () => {};
+
+  onInputchange = e => this.setState({ [e.target.id]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    if (Object.keys(errors).length > 0) return this.setState({ errors });
+
+    // TODO: send transaction
+  };
+
+  // Perform validations and return an object of type { fieldId: [String] }
+  validate = () => {
+    const { toAddress } = this.state;
+    const errors = {};
+
+    // validations for address field
+    if (!toAddress) {
+      errors.toAddress = 'Address is required';
+    } else if (!Web3.utils.isAddress(this.state.toAddress)) {
+      errors.toAddress = 'Invalid address';
+    }
+
+    // TODO: other validations
+
+    return errors;
+  };
+
   render() {
-    // const { foo } = this.state;
+    const {
+      toAddress,
+      ethAmount,
+      usdAmount,
+      ethFee,
+      usdFee,
+      errors
+    } = this.state;
 
     return (
-      <Container>
-        <Sp pt={4} pb={3} px={3}>
-          <Field>
-            <Label>Sent to Address</Label>
-            <Input placeholder="e.g. 0x2345678998765434567" type="text" />
-          </Field>
-          <Row>
-            <Field>
-              <Label>Amount (ETH)</Label>
-              <Input placeholder="0.00" type="text" />
-            </Field>
-            <Field>
-              <Label>Amount (USD)</Label>
-              <Input placeholder="$0.00" type="text" />
-            </Field>
-          </Row>
-          <Row>
-            <Field>
-              <Label>Fee (ETH)</Label>
-              <Input placeholder="0.00" type="text" />
-            </Field>
-            <Field>
-              <Label>Amount (USD)</Label>
-              <Input placeholder="$0.00" type="text" />
-            </Field>
-          </Row>
-        </Sp>
-        <BtnContainer>
-          <Btn block submit>
-            Review Send
-          </Btn>
-        </BtnContainer>
-      </Container>
+      <Flex.Column grow="1">
+        <form onSubmit={this.onSubmit}>
+          <Sp pt={4} pb={3} px={3}>
+            <TextInput
+              placeholder="e.g. 0x2345678998765434567"
+              onChange={this.onInputchange}
+              error={errors.toAddress}
+              label="Send to Address"
+              value={toAddress}
+              id="toAddress"
+            />
+            <Sp mt={3}>
+              <Flex.Row justify="space-between">
+                <Flex.Item grow="1" basis="0">
+                  <MaxBtn onClick={this.onMaxClick}>MAX</MaxBtn>
+                  <TextInput
+                    placeholder="0.00"
+                    onChange={this.onInputchange}
+                    label="Amount (ETH)"
+                    value={ethAmount}
+                    error={errors.ethAmount}
+                    id="ethAmount"
+                  />
+                </Flex.Item>
+                <Sp mt={6} mx={1}>
+                  <TxIcon />
+                </Sp>
+                <Flex.Item grow="1" basis="0">
+                  <TextInput
+                    placeholder="0.00"
+                    onChange={this.onInputchange}
+                    label="Amount (USD)"
+                    value={usdAmount}
+                    error={errors.usdAmount}
+                    id="usdAmount"
+                  />
+                </Flex.Item>
+              </Flex.Row>
+            </Sp>
+            <Sp mt={3}>
+              <Flex.Row justify="space-between">
+                <Flex.Item grow="1" basis="0">
+                  <TextInput
+                    placeholder="0.00"
+                    onChange={this.onInputchange}
+                    label="Fee (ETH)"
+                    value={ethFee}
+                    error={errors.ethFee}
+                    id="ethFee"
+                  />
+                </Flex.Item>
+                <Sp mt={6} mx={1}>
+                  <TxIcon />
+                </Sp>
+                <Flex.Item grow="1" basis="0">
+                  <TextInput
+                    placeholder="0.00"
+                    onChange={this.onInputchange}
+                    label="Amount (USD)"
+                    value={usdFee}
+                    error={errors.usdFee}
+                    id="usdFee"
+                  />
+                </Flex.Item>
+              </Flex.Row>
+            </Sp>
+          </Sp>
+          <BtnContainer>
+            <Btn block submit>
+              Review Send
+            </Btn>
+          </BtnContainer>
+        </form>
+      </Flex.Column>
     );
   }
 }
