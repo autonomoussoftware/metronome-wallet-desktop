569514ef7c32110890a666dd8a8504805cace31c
Pablo Enrici <pablo@bloq.com>
2018-02-02 14:29:24 -0300

    Add Redux devtools

--- electron-starter.js
+++ electron-starter.js
@@ -1,20 +1,22 @@
 const {
   default: installExtension,
-  REACT_DEVELOPER_TOOLS
-} = require('electron-devtools-installer');
-require('electron-debug')({ enabled: true });
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+  REACT_DEVELOPER_TOOLS,
+  REDUX_DEVTOOLS
+} = require("electron-devtools-installer")
+require("electron-debug")({ enabled: true })
+const electron = require("electron")
 
-const path = require('path');
-const url = require('url');
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require("path")
+const url = require("url")
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow
 
 function createWindow() {
   // Create the browser window.
@@ -22,55 +24,55 @@ function createWindow() {
     // titleBarStyle: 'hidden',
     width: 1140,
     height: 700
-  });
+  })
 
   // and load the index.html of the app.
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
-      pathname: path.join(__dirname, './build/index.html'),
-      protocol: 'file:',
+      pathname: path.join(__dirname, "./build/index.html"),
+      protocol: "file:",
       slashes: true
-    });
-  mainWindow.loadURL(startUrl);
+    })
+  mainWindow.loadURL(startUrl)
 
-  installExtension(REACT_DEVELOPER_TOOLS)
+  installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
     .then(name => console.log(`Added Extension:  ${name}`))
-    .catch(err => console.log('An error occurred: ', err));
+    .catch(err => console.log("An error occurred: ", err))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on("closed", function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on("window-all-closed", function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
+  if (process.platform !== "darwin") {
+    app.quit()
   }
-});
+})
 
-app.on('activate', function() {
+app.on("activate", function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
