67e36e0b48ccf225d51e3b59cdd7df8a7797e1e0
Gabriel Montes <gabriel@bloq.com>
2018-02-06 09:54:32 -0300

    Fix error installing devtools

--- src/main/initDev.js
+++ src/main/initDev.js
@@ -1,13 +1,17 @@
 require('electron-debug')({ enabled: true })
 
-const {
-  default: installExtension,
-  REACT_DEVELOPER_TOOLS,
-  REDUX_DEVTOOLS
-} = require('electron-devtools-installer')
+const { app } = require('electron')
 
-installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
-  .then(name => console.log(`Added Extension:  ${name}`))
-  .catch(err => console.log('An error occurred: ', err))
+app.on('ready', function () {
+  const {
+    default: installExtension,
+    REACT_DEVELOPER_TOOLS,
+    REDUX_DEVTOOLS
+  } = require('electron-devtools-installer')
+
+  installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
+    .then(name => console.log(`Added Extension:  ${name}`))
+    .catch(err => console.log('An error occurred: ', err))
+})
 
 require('./init')
