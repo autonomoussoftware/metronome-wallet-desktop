0108076f2f88635f9f7f2b1d29d5874a824aced3
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-02-20 15:11:22 -0300

    Add sentry to main process and UI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+import React from 'react'
+import Raven from 'raven-js'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import { ThemeProvider } from 'styled-components'
-import { Provider } from 'react-redux'
-import createStore from './createStore'
-import ReactDOM from 'react-dom'
+
 import theme from './theme'
-import React from 'react'
+import config from './config'
 import App from './components/App'
+import createStore from './createStore'
+
+if (config.SENTRY_DSN) {
+  Raven.config(
+    'https://d99caae5ca1b495584e5aa3187966f30@sentry.io/277183'
+  ).install()
+
+  window.addEventListener('unhandledrejection', function(e) {
+    Raven.captureException(e.reason)
+  })
+}
 
 // We could pass some initial state to createStore()
 const store = createStore()
