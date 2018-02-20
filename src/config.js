4094c3d7b9dbd65ffef528844f2f806709f2a4c9
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-02-20 15:34:54 -0300

    Update contract address

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   MTN_TOKEN_ADDR:
     process.env.REACT_APP_MTN_TOKEN_ADDR ||
-    '0xf583c8fe0cbf447727378e3b1e921b1ef81adda8',
+    '0x4c00f8ec2d4fc3d9cbe4d7bd04d80780d5cae77f',
   MTN_EXPLORER_URL:
     process.env.REACT_APP_MTN_EXPLORER_URL ||
     'http://explorer.mtn.bloqrock.net',
