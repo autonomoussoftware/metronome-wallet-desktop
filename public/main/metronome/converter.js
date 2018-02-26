4737695699b94b6e3987e28d1e44b6e1a1b58b39
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-02-26 15:32:38 -0300

    Add gas price support to mtn convert

--- public/main/metronome/converter.js
+++ public/main/metronome/converter.js
@@ -1,20 +1,23 @@
+const logger = require('electron-log')
 const promiseAllProps = require('promise-all-props')
 
 const abi = require('./contracts/AutonomousConverter.75f890c')
 
-function getConverterStatus ({ web3, address }) {
+function getConverterStatus({ web3, address }) {
   const converter = new web3.eth.Contract(abi, address)
 
   const calls = {
     availableMtn: converter.methods.getMtnBalance().call(),
     availableEth: converter.methods.getEthBalance().call(),
-    currentPrice: converter.methods.getEthForMtnResult(web3.utils.toWei('1', 'ether')).call()
+    currentPrice: converter.methods
+      .getEthForMtnResult(web3.utils.toWei('1', 'ether'))
+      .call()
   }
 
   return promiseAllProps(calls)
 }
 
-function encodeConvertEthToMtn ({ web3, address, minReturn = 1 }) {
+function encodeConvertEthToMtn({ web3, address, minReturn = 1 }) {
   const contract = new web3.eth.Contract(abi, address)
   const convert = contract.methods.convertEthToMtn(minReturn)
   const data = convert.encodeABI()
@@ -22,7 +25,7 @@ function encodeConvertEthToMtn ({ web3, address, minReturn = 1 }) {
   return data
 }
 
-function encodeConvertMtnToEth ({ web3, address, value, minReturn = 1 }) {
+function encodeConvertMtnToEth({ web3, address, value, minReturn = 1 }) {
   const contract = new web3.eth.Contract(abi, address)
   const convert = contract.methods.convertMtnToEth(value, minReturn)
   const data = convert.encodeABI()
@@ -42,10 +45,25 @@ function getEthForMtnResult ({ web3, address, value }) {
   return contract.methods.getEthForMtnResult(value).call()
 }
 
+function getGasLimit({ web3, from, address, value, minReturn = 1 }) {
+  logger.verbose('Getting converter gas limit', { address, value, from })
+
+  const contract = new web3.eth.Contract(abi, address)
+  const convertMtnToEth = contract.methods.convertMtnToEth(value, minReturn)
+
+  return convertMtnToEth.estimateGas({ from }).then(gasLimit => {
+    logger.verbose('Converter gas limit retrieved', gasLimit)
+
+    return { gasLimit }
+  })
+}
+
+
 module.exports = {
   encodeConvertEthToMtn,
   encodeConvertMtnToEth,
   getConverterStatus,
   getMtnForEthResult,
   getEthForMtnResult
+  getGasLimit
 }
