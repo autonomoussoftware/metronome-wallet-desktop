9ef3ef48eaf16f60f5745ca85ac42b192b240e3e
Gabriel Montes <gabriel@bloq.com>
2018-02-26 17:30:22 -0300

    Refactor main settings logic

--- public/main/settings.js
+++ public/main/settings.js
@@ -1,41 +1,78 @@
-const { app, ipcMain } = require('electron')
 const { merge, unset } = require('lodash')
 const logger = require('electron-log')
 const settings = require('electron-settings')
 
-function presetDefault () {
+const { restart } = require('./lib/electron-restart')
+
+const settableSettings = [
+  'app.node.websocketApiUrl'
+]
+
+const overwritableSettings = [
+  'app.bestBlock',
+  'metronome.contracts',
+  'tokens.0xf583c8fe0cbf447727378e3b1e921b1ef81adda8'
+]
+
+function getKey (key) {
+  return settings.get(key)
+}
+
+function setKey (key, value) {
+  logger.verbose('Settings changed', key)
+  return settings.set(key, value)
+}
+
+function presetDefaults () {
   logger.verbose('Settings file', settings.file())
 
   const currentSettings = settings.getAll()
   const defaultSettings = require('./defaultSettings')
 
-  // Clear previous settings on settings version change
+  // Clear previous settings if settings file version changed
   if (currentSettings.settingsVersion !== defaultSettings.settingsVersion) {
-    [
-      'app.bestBlock',
-      'metronome.contracts',
-      'tokens.0xf583c8fe0cbf447727378e3b1e921b1ef81adda8'
-    ].forEach(function (prop) {
+    logger.verbose('Updating default settings')
+    overwritableSettings.forEach(function (prop) {
       unset(currentSettings, prop)
     })
   }
 
   settings.setAll(merge(defaultSettings, currentSettings))
-
-  logger.silly('Current settings', settings.getAll())
+  logger.verbose('Default settings applied')
+  logger.debug('Current settings', settings.getAll())
 }
 
-ipcMain.on('settings-get', function (event, { key }) {
-  event.returnValue = settings.get(key)
-})
+function attachSync (ipcMain) {
+  ipcMain.on('settings-get', function (event, { key }) {
+    event.returnValue = getKey(key)
+  })
 
-ipcMain.on('settings-set', function (event, { key, value }) {
-  // TODO: add settings whitelist
-  settings.set(key, value)
-  event.returnValue = true
+  ipcMain.on('settings-set', function (event, { key, value }) {
+    if (!settableSettings.includes(key)) {
+      event.returnValue = false
+      return
+    }
 
-  app.relaunch({args: process.argv.slice(1).concat(['--relaunch'])})
-  app.exit(0)
-})
+    setKey(key, value)
+    event.returnValue = true
 
-module.exports = { presetDefault }
+    restart()
+  })
+}
+
+function getPasswordHash () {
+  return getKey(`user.passwordHash`)
+}
+
+function setPasswordHash (hash) {
+  setKey(`user.passwordHash`, hash)
+}
+
+module.exports = {
+  attachSync,
+  getKey,
+  getPasswordHash,
+  presetDefaults,
+  setKey,
+  setPasswordHash
+}
