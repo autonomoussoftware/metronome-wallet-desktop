41e311d4dd896471cd408553066936ce1ffae35b
Gabriel Montes <gabriel@bloq.com>
2018-03-19 20:42:22 -0300

    Remove magic number in auctions gas estimation

--- public/main/plugins/metronome/auctions.js
+++ public/main/plugins/metronome/auctions.js
@@ -10,16 +10,12 @@ function getAuctionStatus ({ web3, address }) {
   const auctions = new web3.eth.Contract(auctionsAbi, address)
 
   const calls = {
-    genesisTime: auctions.methods
-      .genesisTime()
-      .call()
+    genesisTime: auctions.methods.genesisTime().call()
       .then(t => Number.parseInt(t, 10)),
     currentPrice: auctions.methods.currentPrice().call(),
     tokenRemaining: auctions.methods.heartbeat().call()
       .then(get('[4]')),
-    nextAuctionStartTime: auctions.methods
-      .nextAuction()
-      .call()
+    nextAuctionStartTime: auctions.methods.nextAuction().call()
       .then(data => data._startTime)
       .then(t => Number.parseInt(t, 10)),
     currentAuction: auctions.methods.currentAuction().call(),
@@ -33,9 +29,9 @@ function getAuctionGasLimit ({ web3, from, to, value }) {
   logger.verbose('Getting auction limit gas for address: ', { from, to, value })
 
   // TODO: temp fix to avoid underestimation
-  return web3
-    .eth.estimateGas({ from, to, value })
-    .then(gasLimit => ({ gasLimit: Math.ceil(gasLimit * 1.1) }))
+  const MULT = 1.1
+  return web3.eth.estimateGas({ from, to, value })
+    .then(gasLimit => ({ gasLimit: Math.ceil(gasLimit * MULT) }))
 }
 
 module.exports = { getAuctionStatus, getAuctionGasLimit }
