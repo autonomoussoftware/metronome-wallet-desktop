f587a33ea18ad1031617950f669ca0d692b51450
Gabriel Montes <gabriel@bloq.com>
2018-04-28 23:24:49 -0300

    Fix styles and linter warnings

--- public/main/mainWindow.js
+++ public/main/mainWindow.js
@@ -11,6 +11,58 @@ const restart = require('./electron-restart')
 
 let mainWindow
 
+function showUpdateNotification (info = {}) {
+  const restartNowAction = 'Restart now'
+
+  const versionLabel = info.label
+    ? `Version ${info.version}`
+    : 'The latest version'
+
+  notifier.notify(
+    {
+      title: 'A new update is ready to install.',
+      wait: true,
+      sound: true,
+      message: `${versionLabel} will be automatically installed after restart.`,
+      closeLabel: 'Ok',
+      actions: restartNowAction
+    },
+    function (err) {
+      if (err) throw err
+      autoUpdater.quitAndInstall()
+    }
+  )
+}
+
+function initAutoUpdate () {
+  if (isDev) { return }
+  if (process.platform === 'linux') { return }
+
+  autoUpdater.checkForUpdates()
+
+  autoUpdater.on('checking-for-update', function () {
+    logger.info('Checking for update...')
+  })
+  autoUpdater.on('update-available', function () {
+    logger.info('Update available.')
+  })
+  autoUpdater.on('update-not-available', function () {
+    logger.info('Update not available.')
+  })
+  autoUpdater.on('error', function (err) {
+    logger.error(`Error in auto-updater. ${err}`)
+  })
+  autoUpdater.on('download-progress', function (progressObj) {
+    let msg = `Download speed: ${progressObj.bytesPerSecond}`
+    msg += ` - Downloaded ${progressObj.percent}%`
+    msg += ` (${progressObj.transferred}/${progressObj.total})`
+    logger.info(msg)
+  })
+  autoUpdater.on('update-downloaded', function (info) {
+    showUpdateNotification(info)
+  })
+}
+
 function loadWindow () {
   // ensure the app is ready before creating the main window
   if (!app.isReady()) {
@@ -53,60 +105,9 @@ function loadWindow () {
     mainWindow = null
   })
 
-  mainWindow.once('ready-to-show', () => {
+  mainWindow.once('ready-to-show', function () {
     mainWindow.show()
   })
-}
-
-function initAutoUpdate () {
-  if (isDev) { return }
-  if (process.platform === 'linux') { return }
-
-  autoUpdater.checkForUpdates()
-
-  autoUpdater.on('checking-for-update', () => {
-    logger.info('Checking for update...')
-  })
-  autoUpdater.on('update-available', () => {
-    logger.info('Update available.')
-  })
-  autoUpdater.on('update-not-available', () => {
-    logger.info('Update not available.')
-  })
-  autoUpdater.on('error', err => {
-    logger.error(`Error in auto-updater. ${err}`)
-  })
-  autoUpdater.on('download-progress', progressObj => {
-    let msg = `Download speed: ${progressObj.bytesPerSecond}`
-    msg += ` - Downloaded ${progressObj.percent}%`
-    msg += ` (${progressObj.transferred}/${progressObj.total})`
-    logger.info(msg)
-  })
-  autoUpdater.on('update-downloaded', info => {
-    showUpdateNotification(info)
-  })
-}
-
-function showUpdateNotification (info) {
-  info = info || {}
-  const restartNowAction = 'Restart now'
-
-  const versionLabel = info.label ? `Version ${info.version}` : 'The latest version'
-
-  notifier.notify(
-    {
-      title: 'A new update is ready to install.',
-      wait: true,
-      sound: true,
-      message: `${versionLabel} has been downloaded and will be automatically installed after restart.`,
-      closeLabel: 'Ok',
-      actions: restartNowAction
-    },
-    function (err) {
-      if (err) throw err
-      autoUpdater.quitAndInstall()
-    }
-  )
 }
 
 function createWindow () {
