befc54a15c1c5b7b7fe3da710c11e41bea3246ae
Gabriel Montes <gabriel@bloq.com>
2018-05-08 17:18:21 -0300

    Get indexer cookies before subscribing

--- public/main/plugins/bloq-eth-explorer/index.js
+++ public/main/plugins/bloq-eth-explorer/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const { CookieJar } = require('tough-cookie')
+const axios = require('axios')
+const axiosCookieJarSupport = require('axios-cookiejar-support').default
 const io = require('socket.io-client')
 const logger = require('electron-log')
 
@@ -8,49 +11,79 @@ const createBasePlugin = require('../../base-plugin')
 const { getIndexerApiUrl } = require('./settings')
 const api = require('./api')
 
+axiosCookieJarSupport(axios)
+
+let socket
+
 const baseURL = getIndexerApiUrl()
 
-const socket = io(`${baseURL}/v1`, { autoConnect: false })
+const jar = new CookieJar()
 
-function subscribeBlocks (eventsBus) {
-  socket.emit('subscribe', { type: 'blocks' }, function (err) {
-    if (err) {
-      logger.warn('Blocks subscription failed', err)
-      return
-    }
+const setTimeoutAsync = timeout => new Promise(function (resolve) {
+  setTimeout(resolve, timeout)
+})
 
-    logger.debug('Blocks subscription successfull')
+const getCookie = () => axios.get(baseURL, { jar, withCredentials: true })
+  .then(function () {
+    socket = io(`${baseURL}/v1`, {
+      autoConnect: false,
+      extraHeaders: {
+        Cookie: jar.getCookiesSync(baseURL).join(';')
+      }
+    })
+  })
+  .catch(function (err) {
+    logger.warn('Failed to get subscription cookie', err.message)
+
+    return setTimeoutAsync(5000)
+      .then(getCookie)
   })
 
-  socket.on('block', function (data) {
-    logger.verbose('New block received', data)
+const initialized = getCookie()
 
-    eventsBus.emit('new-best-block', data)
+function subscribeBlocks (eventsBus) {
+  initialized.then(function () {
+    socket.emit('subscribe', { type: 'blocks' }, function (err) {
+      if (err) {
+        logger.warn('Blocks subscription failed', err)
+        return
+      }
+
+      logger.debug('Blocks subscription successfull')
+    })
+
+    socket.on('block', function (data) {
+      logger.verbose('New block received', data)
+
+      eventsBus.emit('new-best-block', data)
+    })
   })
 }
 
 const start = eventsBus => function (pluginEmitter) {
-  socket.on('error', function (err) {
-    logger.warn('Connection error', err.message)
+  initialized.then(function () {
+    socket.on('error', function (err) {
+      logger.warn('Connection error', err.message)
 
-    pluginEmitter.emit('connection-state-changed', 'error')
+      pluginEmitter.emit('connection-state-changed', 'error')
+    })
+
+    socket.on('connect', function () {
+      logger.debug('Client connected')
+
+      pluginEmitter.emit('connection-state-changed', 'connected')
+
+      subscribeBlocks(eventsBus)
+    })
+
+    socket.on('disconnect', function (reason) {
+      logger.debug('Connection closed', reason)
+
+      pluginEmitter.emit('connection-state-changed', 'disconnected')
+    })
+
+    socket.open()
   })
-
-  socket.on('connect', function () {
-    logger.debug('Client connected')
-
-    pluginEmitter.emit('connection-state-changed', 'connected')
-
-    subscribeBlocks(eventsBus)
-  })
-
-  socket.on('disconnect', function (reason) {
-    logger.debug('Connection closed', reason)
-
-    pluginEmitter.emit('connection-state-changed', 'disconnected')
-  })
-
-  socket.open()
 }
 
 function broadcastConnectivityState (subscriptions, state) {
@@ -64,40 +97,46 @@ function broadcastConnectivityState (subscriptions, state) {
 }
 
 function stop () {
-  socket.removeAllListeners('error')
+  initialized.then(function () {
+    socket.removeAllListeners('error')
 
-  socket.removeAllListeners('connect')
+    socket.removeAllListeners('connect')
 
-  socket.removeAllListeners('disconnect')
+    socket.removeAllListeners('disconnect')
 
-  socket.close()
+    socket.close()
+  })
 }
 
 function subscribeAddresses ({ eventsBus, walletId, addresses }) {
-  socket.emit('subscribe', { type: 'txs', addresses }, function (err) {
-    if (err) {
-      logger.warn('Subscription failed', walletId, err)
-      return
-    }
+  initialized.then(function () {
+    socket.emit('subscribe', { type: 'txs', addresses }, function (err) {
+      if (err) {
+        logger.warn('Subscription failed', walletId, err)
+        return
+      }
 
-    logger.debug('Subscription successfull', walletId)
-  })
+      logger.debug('Addresses subscription successfull', addresses, walletId)
+    })
 
-  socket.on('tx', function (data) {
-    const { type, txid, status, meta } = data
+    socket.on('tx', function (data) {
+      const { type, txid, status, meta } = data
 
-    logger.verbose('New transaction received', { walletId, data })
+      logger.verbose('New transaction received', { walletId, data })
 
-    eventsBus.emit(`${type}-tx-${status}`, { walletId, txid, meta })
+      eventsBus.emit(`${type}-tx-${status}`, { walletId, txid, meta })
+    })
   })
 }
 
 function attachToEvents (eventsBus) {
   eventsBus.on('wallet-opened', function ({ walletId, addresses }) {
-    subscribeAddresses({ eventsBus, walletId, addresses })
-
-    socket.on('connect', function () {
+    initialized.then(function () {
       subscribeAddresses({ eventsBus, walletId, addresses })
+
+      socket.on('connect', function () {
+        subscribeAddresses({ eventsBus, walletId, addresses })
+      })
     })
   })
 }
