c51385db261b7a235a789a41f0307e1570c4151b
Gabriel Montes <gabriel@bloq.com>
2018-05-17 13:14:20 -0300

    Refactor auction status to avoid duplicated calls

--- public/main/plugins/metronome/auctions.js
+++ public/main/plugins/metronome/auctions.js
@@ -2,31 +2,47 @@
 
 const logger = require('electron-log')
 const promiseAllProps = require('promise-all-props')
-const { get } = require('lodash/fp')
 
 const auctionsAbi = require('./contracts/Auctions')
+
+function parseAuctionStatus (status) {
+  const {
+    currentAuction,
+    currentPrice,
+    genesisTime,
+    heartbeat: {
+      minting,
+      nextAuctionGMT
+    },
+    isInitialAuctionEnded
+  } = status
+
+  return {
+    currentAuction,
+    currentPrice,
+    genesisTime: Number.parseInt(genesisTime, 10),
+    isInitialAuctionEnded,
+    nextAuctionStartTime: Number.parseInt(nextAuctionGMT, 10),
+    tokenRemaining: minting
+  }
+}
 
 function getAuctionStatus ({ web3, address }) {
   const auctions = new web3.eth.Contract(auctionsAbi, address)
 
   const calls = {
-    genesisTime: auctions.methods.genesisTime().call()
-      .then(t => Number.parseInt(t, 10)),
+    genesisTime: auctions.methods.genesisTime().call(),
     currentPrice: auctions.methods.currentPrice().call()
       .catch(() => 0),
-    tokenRemaining: auctions.methods.heartbeat().call()
-      .catch(() => ({ minting: 0 }))
-      .then(get('minting')), // [4]
-    nextAuctionStartTime: auctions.methods.heartbeat().call()
-      .catch(() => ({ nextAuctionGMT: 0 }))
-      .then(get('nextAuctionGMT')) // [9]
-      .then(t => Number.parseInt(t, 10)),
+    heartbeat: auctions.methods.heartbeat().call()
+      .catch(() => ({ minting: 0, nextAuctionGMT: 0 })),
     currentAuction: auctions.methods.currentAuction().call()
       .catch(() => 0),
     isInitialAuctionEnded: auctions.methods.isInitialAuctionEnded().call()
   }
 
   return promiseAllProps(calls)
+    .then(parseAuctionStatus)
 }
 
 function getAuctionGasLimit ({ web3, from, to, value }) {
