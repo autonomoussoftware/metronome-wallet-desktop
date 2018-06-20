513b9c5e5c6eb768be2c56e9fcb0f7e741067581
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-06-20 11:20:25 -0300

    Update sentry dns

--- public/config.js
+++ public/config.js
@@ -4,8 +4,7 @@
 
 const config = {
   sentryDsn:
-    process.env.SENTRY_DSN ||
-    'https://d9905c2eec994071935593d4085d3547:87dffc4df35a436bb59ec1e7c8228722@sentry.io/290706'
+    process.env.SENTRY_DSN || 'https://d9905c2eec994071935593d4085d3547@sentry.io/290706'
 }
 
 module.exports = config
