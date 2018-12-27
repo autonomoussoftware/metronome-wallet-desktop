88456bbea2925a687c68cd56e95d2b0b01221ba4
Pablo Enrici <pablo@bloq.com>
2018-12-27 18:00:03 -0300

    Display status of all enabled chains un Loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,44 +3,71 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import React from 'react'
 
-import { LoadingBar, AltLayout } from './common'
+import { LoadingBar, AltLayout, Flex } from './common'
 import ChecklistItem from './common/ChecklistItem'
 
+const ChecklistContainer = styled(Flex.Row)`
+  margin: 4rem -20rem;
+`
+
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 0.9rem;
+  letter-spacing: 1.6px;
+  opacity: 0.5;
+  margin-bottom: 0.8rem;
+  padding-left: 8rem;
+`
+
 const Checklist = styled.div`
-  margin-top: 3.2rem;
   padding-left: 4.8rem;
 `
 
 class Loading extends React.Component {
   static propTypes = {
-    hasBlockHeight: PropTypes.bool.isRequired,
-    hasCoinBalance: PropTypes.bool.isRequired,
-    hasMetBalance: PropTypes.bool.isRequired,
-    hasCoinRate: PropTypes.bool.isRequired
+    chainsStatus: PropTypes.objectOf(
+      PropTypes.shape({
+        hasBlockHeight: PropTypes.bool,
+        hasCoinBalance: PropTypes.bool,
+        hasMetBalance: PropTypes.bool,
+        hasCoinRate: PropTypes.bool,
+        displayName: PropTypes.string.isRequired,
+        symbol: PropTypes.string.isRequired
+      })
+    ).isRequired
   }
 
   render() {
     return (
       <AltLayout title="Gathering Information..." data-testid="loading-scene">
         <LoadingBar />
-        <Checklist>
-          <ChecklistItem
-            isActive={this.props.hasBlockHeight}
-            text="Blockchain status"
-          />
-          <ChecklistItem
-            isActive={this.props.hasCoinRate}
-            text="ETH exchange data"
-          />
-          <ChecklistItem
-            isActive={this.props.hasCoinBalance}
-            text="ETH balance"
-          />
-          <ChecklistItem
-            isActive={this.props.hasMetBalance}
-            text="MET balance"
-          />
-        </Checklist>
+        <ChecklistContainer justify="center">
+          {Object.keys(this.props.chainsStatus).map(chainName => (
+            <div key={chainName}>
+              <Title>{this.props.chainsStatus[chainName].displayName}</Title>
+              <Checklist>
+                <ChecklistItem
+                  isActive={this.props.chainsStatus[chainName].hasBlockHeight}
+                  text="Blockchain status"
+                />
+                <ChecklistItem
+                  isActive={this.props.chainsStatus[chainName].hasCoinRate}
+                  text={`${
+                    this.props.chainsStatus[chainName].symbol
+                  } exchange data`}
+                />
+                <ChecklistItem
+                  isActive={this.props.chainsStatus[chainName].hasCoinBalance}
+                  text={`${this.props.chainsStatus[chainName].symbol} balance`}
+                />
+                <ChecklistItem
+                  isActive={this.props.chainsStatus[chainName].hasMetBalance}
+                  text="MET balance"
+                />
+              </Checklist>
+            </div>
+          ))}
+        </ChecklistContainer>
       </AltLayout>
     )
   }
