934f2be44479e476f56d63564dc447b12e43ca51
Pablo Enrici <pablo@bloq.com>
2019-02-01 17:02:22 -0300

    Refactor Redux subscriptions to Main Process messages
    Some of the checks were not actually needed!

--- src/subscriptions.js
+++ src/subscriptions.js
@@ -1,9 +1,5 @@
-export const subscribeToMainProcessMessages = store => {
-  /**
-   * Subscribe to an array of IPC messages and dispatch a Redux
-   * action of type { type: MSG_CHANNEL, payload: MSG_ARG }
-   */
-  subscribeTo([
+export function subscribeToMainProcessMessages(store) {
+  const ipcMessages = [
     'metronome-token-status-updated',
     'web3-connection-status-changed',
     'connectivity-state-changed',
@@ -16,27 +12,13 @@ export const subscribeToMainProcessMessages = store => {
     'create-wallet',
     'open-wallets',
     'coin-block'
-  ])
+  ]
 
-  /*
-   * For more complex subscriptions you can do the following
-   *
-   * window.ipcRenderer.on(MSG_CHANNEL, (event, arg) => {
-   *   do something with event and arg here...
-   * })
-   */
-  function subscribeTo(types) {
-    return types.forEach(type =>
-      window.ipcRenderer.on(type, (ev, { id, data, ...other }) => {
-        // ignore messages returned as promises with errors
-        if (id && data && data.error) return
-
-        if (id && data && !data.error) {
-          store.dispatch({ type, payload: data })
-        } else {
-          store.dispatch({ type, payload: { ...other } })
-        }
-      })
+  // Subscribe to every IPC message defined above and dispatch a
+  // Redux action of type { type: MSG_NAME, payload: MSG_ARG }
+  ipcMessages.forEach(msgName =>
+    window.ipcRenderer.on(msgName, (_, payload) =>
+      store.dispatch({ type: msgName, payload })
     )
-  }
+  )
 }
