385d91c0e2e8fa1662e8994703b5f3de24f2ef7d
Pablo Enrici <pablo@bloq.com>
2019-02-18 21:10:33 -0300

    Implement displaying ongoing ports

---
+++ src/components/port/OngoingImports.js
@@ -0,0 +1,121 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import React from 'react'
+
+import { DisplayValue, Flex } from '../common'
+
+const List = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin-left: 0;
+  margin-right: 0;
+  margin-top: 2.4rem;
+  margin-bottom: 4.8rem;
+`
+
+const Item = styled.li`
+  background-color: ${({ theme }) => theme.colors.lightShade};
+  border-radius: 2px;
+  padding: 1.2rem 2.4rem 1.2rem 1.6rem;
+
+  & + & {
+    margin-top: 1.6rem;
+  }
+`
+
+const LeftLabel = styled.div`
+  font-size: 1.1rem;
+  letter-spacing: 0.4px;
+  color: #c2c4c6;
+`
+
+const Hiddable = styled.span`
+  display: none;
+
+  @media (min-width: 1000px) {
+    display: inline;
+  }
+`
+
+const Validations = styled.span`
+  background-color: ${({ theme }) => theme.colors.darkShade};
+  color: ${({ theme }) => theme.colors.light};
+  padding: 0.5rem 1.2rem;
+  border-radius: 1.2rem;
+  margin-right: 1.6rem;
+`
+
+const Refutations = styled.span`
+  color: ${({ theme }) => theme.colors.warning};
+  padding: 0.5rem 1.2rem;
+  margin-right: 1.6rem;
+`
+
+const Amount = styled.div`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 2rem;
+  font-weight: 600;
+`
+
+const Details = styled.div`
+  font-size: 1.1rem;
+  letter-spacing: 0.4px;
+  color: #c2c4c6;
+
+  & > span {
+    color: ${({ theme }) => theme.colors.light};
+    font-size: 1.3rem;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+`
+
+export default class OngoingImports extends React.Component {
+  static propTypes = {
+    attestationThreshold: PropTypes.number.isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        attestedCount: PropTypes.number.isRequired,
+        refutedCount: PropTypes.number.isRequired,
+        importedFrom: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+        hash: PropTypes.string.isRequired
+      })
+    ).isRequired
+  }
+
+  render() {
+    return (
+      <List>
+        {this.props.items.map(item => (
+          <Item key={item.hash}>
+            <Flex.Row justify="space-between" align="center">
+              <LeftLabel>
+                <Validations>
+                  {item.attestedCount} / {this.props.attestationThreshold}
+                </Validations>{' '}
+                <Hiddable>VALIDATIONS</Hiddable>{' '}
+                {item.refutedCount > 0 && (
+                  <Refutations>
+                    {item.refutedCount}{' '}
+                    <Hiddable>
+                      {item.refutedCount > 1 ? 'REFUTATIONS' : 'REFUTATION'}
+                    </Hiddable>
+                  </Refutations>
+                )}
+              </LeftLabel>
+              <Flex.Column align="flex-end">
+                <Amount>
+                  <DisplayValue value={item.value} post=" MET" />
+                </Amount>
+                <Details>
+                  IMPORTING FROM <span>{item.importedFrom}</span> BLOCKCHAIN
+                </Details>
+              </Flex.Column>
+            </Flex.Row>
+          </Item>
+        ))}
+      </List>
+    )
+  }
+}
--- src/components/port/Port.js
+++ src/components/port/Port.js
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import React from 'react'
 
-import { DarkLayout, DisplayValue, Flex, Btn } from '../common'
+import { DarkLayout, Flex, Btn } from '../common'
 import RetryImportDrawer from './RetryImportDrawer'
+import OngoingImports from './OngoingImports'
 import FailedImports from './FailedImports'
 import PortDrawer from './PortDrawer'
 
@@ -33,66 +34,11 @@ const Description = styled.p`
   color: #c2c4c6;
   margin-top: 0.8rem;
   margin-bottom: 0;
-`
-
-const List = styled.ul`
-  list-style-type: none;
-  padding: 0;
-  margin-left: 0;
-  margin-right: 0;
-  margin-top: 2.4rem;
-  margin-bottom: 4.8rem;
-`
-
-const Item = styled.li`
-  background-color: ${({ theme }) => theme.colors.lightShade};
-  border-radius: 2px;
-  padding: 1.2rem 2.4rem 1.2rem 1.6rem;
-
-  & + & {
-    margin-top: 1.6rem;
-  }
-`
-
-const LeftLabel = styled.div`
-  font-size: 1.4rem;
-  letter-spacing: 0.4px;
-  color: #c2c4c6;
-`
-
-const Hiddable = styled.span`
-  display: none;
-
-  @media (min-width: 1000px) {
-    display: inline;
-  }
-`
-
-const Validations = styled.span`
-  background-color: ${({ theme }) => theme.colors.darkShade};
-  color: ${({ theme }) => theme.colors.light};
-  padding: 0.5rem 1.2rem;
-  border-radius: 1.2rem;
-  margin-right: 2.4rem;
-  font-size: 1.1rem;
-`
-
-const Amount = styled.div`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: 2rem;
-  font-weight: 600;
-`
-
-const Details = styled.div`
-  font-size: 1.1rem;
-  letter-spacing: 0.4px;
-  color: #c2c4c6;
 
   & > span {
     color: ${({ theme }) => theme.colors.light};
     font-size: 1.3rem;
     font-weight: 600;
-    text-transform: uppercase;
   }
 `
 
@@ -102,7 +48,7 @@ const BtnContainer = styled.div`
   align-self: center;
 
   @media (min-width: 1000px) {
-    margin-top: 5rem;
+    margin-top: 7.2rem;
     margin-left: 2.4rem;
     margin-bottom: 0;
     order: 0;
@@ -116,16 +62,16 @@ const PortBtn = styled(Btn)`
 
 class Port extends React.Component {
   static propTypes = {
+    attestationThreshold: PropTypes.number.isRequired,
     retryDisabledReason: PropTypes.string,
     portDisabledReason: PropTypes.string,
     shouldRenderForm: PropTypes.bool.isRequired,
-    pendingImports: PropTypes.arrayOf(
+    ongoingImports: PropTypes.arrayOf(
       PropTypes.shape({
-        destinationChain: PropTypes.string.isRequired,
-        totalValidators: PropTypes.number.isRequired,
-        validations: PropTypes.number.isRequired,
-        originChain: PropTypes.string.isRequired,
-        amount: PropTypes.string.isRequired,
+        attestedCount: PropTypes.number.isRequired,
+        refutedCount: PropTypes.number.isRequired,
+        importedFrom: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
         hash: PropTypes.string.isRequired
       })
     ).isRequired,
@@ -162,40 +108,12 @@ class Port extends React.Component {
       <DarkLayout data-testid="port-container" title="Port">
         <Container>
           <Flex.Item grow="1">
-            {this.props.pendingImports.length > 0 && (
-              <React.Fragment>
-                <Title>Pending Imports</Title>
-                <List>
-                  {this.props.pendingImports.map(item => (
-                    <Item key={item.hash}>
-                      <Flex.Row justify="space-between" align="center">
-                        <LeftLabel>
-                          <Validations>
-                            {item.validations}/{item.totalValidators}
-                          </Validations>{' '}
-                          <Hiddable>VALIDATIONS</Hiddable>
-                        </LeftLabel>
-                        <Flex.Column align="flex-end">
-                          <Amount>
-                            <DisplayValue value={item.amount} post=" MET" />
-                          </Amount>
-                          <Details>
-                            IMPORTING FROM <span>{item.originChain}</span> to{' '}
-                            <span>{item.destinationChain}</span>
-                          </Details>
-                        </Flex.Column>
-                      </Flex.Row>
-                    </Item>
-                  ))}
-                </List>
-              </React.Fragment>
-            )}
             {this.props.failedImports.length > 0 && (
-              <div>
-                <Title>Failed Imports</Title>
+              <React.Fragment>
+                <Title>Failed Ports</Title>
                 <Description>
-                  Resubmit incomplete imports that failed to excecute by
-                  clicking Retry.
+                  Resubmit incomplete ports that failed to excecute by clicking
+                  Retry.
                 </Description>
                 <FailedImports
                   retryDisabledReason={this.props.retryDisabledReason}
@@ -203,7 +121,27 @@ class Port extends React.Component {
                   onRetryClick={this.onRetryClick}
                   items={this.props.failedImports}
                 />
-              </div>
+              </React.Fragment>
+            )}
+
+            {this.props.ongoingImports.length > 0 && (
+              <React.Fragment>
+                <Title>Ongoing Ports</Title>
+                <Description>
+                  An Import Request requires at least{' '}
+                  <span>
+                    {this.props.attestationThreshold}{' '}
+                    {this.props.attestationThreshold > 1
+                      ? 'validations'
+                      : 'validation'}
+                  </span>{' '}
+                  for the MET to be imported on this chain.
+                </Description>
+                <OngoingImports
+                  attestationThreshold={this.props.attestationThreshold}
+                  items={this.props.ongoingImports}
+                />
+              </React.Fragment>
             )}
           </Flex.Item>
 
