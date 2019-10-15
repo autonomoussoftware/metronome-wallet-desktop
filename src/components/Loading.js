ba906c29906e99692a8193d7c20781103caabd5e
Pablo Enrici <pablo@bloq.com>
2019-10-15 17:51:15 -0300

    Redesign Loading screen
      - Display only one checklist item per network
      - Display a 'See details' button if initial load takes longer than a certain time
      - If 'See details' button is clicked, display the status of pending network items

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,80 +1,115 @@
 import withLoadingState from 'metronome-wallet-ui-logic/src/hocs/withLoadingState'
+import useLoadingState from 'metronome-wallet-ui-logic/src/hooks/useLoadingState'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import React from 'react'
 
-import { LoadingBar, AltLayout, Flex } from './common'
+import { LoadingBar, AltLayout } from './common'
 import ChecklistItem from './common/ChecklistItem'
 
-const ChecklistContainer = styled(Flex.Row)`
-  margin: 4rem -20rem;
+const ChecklistContainer = styled.div`
+  margin: 4rem auto 2rem;
+  max-width: 200px;
 `
 
-const Title = styled.div`
-  display: ${p => (p.isMultiChain ? 'block' : 'none')};
-  font-weight: 600;
-  font-size: 0.9rem;
-  letter-spacing: 1.6px;
+const DetailItem = styled.div`
+  opacity: ${p => (p.isActive ? 0.5 : 0.3)};
+  font-size: 12px;
+  margin-top: 4px;
+`
+
+const OkLabel = styled.span`
+  color: ${p => p.theme.colors.success};
+  margin-left: 4px;
+`
+
+const DetailsBtn = styled.button`
+  cursor: pointer;
+  font: inherit;
+  background: none;
+  border: none;
+  color: inherit;
   opacity: 0.5;
-  margin-bottom: 0.8rem;
-  padding-left: 8rem;
+  text-decoration: underline;
+  font-size: 12px;
+  margin: 0 auto;
+  display: block;
+  outline: none;
+
+  &:hover {
+    opacity: 1;
+  }
 `
 
-const Checklist = styled.div`
-  padding-left: ${p => (p.isMultiChain ? '4.8rem' : 0)};
-`
+function Loading(props) {
+  const { handleDetailsClick, isDetailVisible, isBtnVisible } = useLoadingState(
+    props.chainsStatus
+  )
 
-class Loading extends React.Component {
-  static propTypes = {
-    isMultiChain: PropTypes.bool.isRequired,
-    chainsStatus: PropTypes.objectOf(
-      PropTypes.shape({
-        hasBlockHeight: PropTypes.bool,
-        hasCoinBalance: PropTypes.bool,
-        hasMetBalance: PropTypes.bool,
-        hasCoinRate: PropTypes.bool,
-        displayName: PropTypes.string.isRequired,
-        symbol: PropTypes.string.isRequired
-      })
-    ).isRequired
-  }
+  return (
+    <AltLayout title="Gathering Information..." data-testid="loading-scene">
+      <LoadingBar />
+      <ChecklistContainer>
+        {Object.keys(props.chainsStatus).map(chainName => {
+          const {
+            hasBlockHeight,
+            hasCoinBalance,
+            hasMetBalance,
+            hasCoinRate,
+            displayName,
+            symbol
+          } = props.chainsStatus[chainName]
 
-  render() {
-    return (
-      <AltLayout title="Gathering Information..." data-testid="loading-scene">
-        <LoadingBar />
-        <ChecklistContainer justify="center">
-          {Object.keys(this.props.chainsStatus).map(chainName => (
-            <div key={chainName}>
-              <Title isMultiChain={this.props.isMultiChain}>
-                {this.props.chainsStatus[chainName].displayName}
-              </Title>
-              <Checklist isMultiChain={this.props.isMultiChain}>
-                <ChecklistItem
-                  isActive={this.props.chainsStatus[chainName].hasBlockHeight}
-                  text="Blockchain status"
-                />
-                <ChecklistItem
-                  isActive={this.props.chainsStatus[chainName].hasCoinRate}
-                  text={`${
-                    this.props.chainsStatus[chainName].symbol
-                  } exchange data`}
-                />
-                <ChecklistItem
-                  isActive={this.props.chainsStatus[chainName].hasCoinBalance}
-                  text={`${this.props.chainsStatus[chainName].symbol} balance`}
-                />
-                <ChecklistItem
-                  isActive={this.props.chainsStatus[chainName].hasMetBalance}
-                  text="MET balance"
-                />
-              </Checklist>
-            </div>
-          ))}
-        </ChecklistContainer>
-      </AltLayout>
-    )
-  }
+          const isActive =
+            hasBlockHeight && hasCoinBalance && hasMetBalance && hasCoinRate
+
+          return (
+            <ChecklistItem
+              isActive={isActive}
+              text={`${displayName} network`}
+              key={chainName}
+            >
+              {isDetailVisible && !isActive && (
+                <>
+                  <DetailItem isActive={hasBlockHeight}>
+                    Blockchain status {hasBlockHeight && <OkLabel>OK</OkLabel>}
+                  </DetailItem>
+                  <DetailItem isActive={hasCoinRate}>
+                    {symbol} exchange data{' '}
+                    {hasCoinRate && <OkLabel>OK</OkLabel>}
+                  </DetailItem>
+                  <DetailItem isActive={hasCoinBalance}>
+                    {symbol} balance {hasCoinBalance && <OkLabel>OK</OkLabel>}
+                  </DetailItem>
+                  <DetailItem isActive={hasMetBalance}>
+                    MET balance {hasMetBalance && <OkLabel>OK</OkLabel>}
+                  </DetailItem>
+                </>
+              )}
+            </ChecklistItem>
+          )
+        })}
+      </ChecklistContainer>
+      {isBtnVisible && (
+        <DetailsBtn type="button" onClick={handleDetailsClick}>
+          Show details
+        </DetailsBtn>
+      )}
+    </AltLayout>
+  )
+}
+
+Loading.propTypes = {
+  chainsStatus: PropTypes.objectOf(
+    PropTypes.shape({
+      hasBlockHeight: PropTypes.bool,
+      hasCoinBalance: PropTypes.bool,
+      hasMetBalance: PropTypes.bool,
+      hasCoinRate: PropTypes.bool,
+      displayName: PropTypes.string.isRequired,
+      symbol: PropTypes.string.isRequired
+    })
+  ).isRequired
 }
 
 export default withLoadingState(Loading)
--- src/components/common/ChecklistItem.js
+++ src/components/common/ChecklistItem.js
@@ -16,7 +16,7 @@ const Icon = styled.svg`
   top: 1px;
 `
 
-const Label = styled.span`
+const Label = styled.div`
   transition: opacity 0.3s;
   opacity: ${p => (p.isActive ? 1 : 0.5)};
 `
@@ -24,6 +24,7 @@ const Label = styled.span`
 export default class ChecklistItem extends React.Component {
   static propTypes = {
     isActive: PropTypes.bool.isRequired,
+    children: PropTypes.node,
     text: PropTypes.string.isRequired
   }
 
@@ -39,6 +40,7 @@ export default class ChecklistItem extends React.Component {
           <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.88-11.71L10 14.17l-1.88-1.88a.996.996 0 1 0-1.41 1.41l2.59 2.59c.39.39 1.02.39 1.41 0L17.3 9.7a.996.996 0 0 0 0-1.41c-.39-.39-1.03-.39-1.42 0z" />
         </Icon>
         <Label isActive={this.props.isActive}>{this.props.text}</Label>
+        {this.props.children}
       </Container>
     )
   }
