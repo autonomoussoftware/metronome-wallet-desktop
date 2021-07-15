4c8efb5b8a3df96db58b3d090169f05d8d8e31de
Gonzalo D'Elia <gonzalo.n.delia@gmail.com>
2021-07-15 11:15:20 -0300

    Add notarization process to publishing Mac's dmg

---
+++ scripts/notarize.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return notarize({
+    appBundleId: 'sh.autonomous.metronome.wallet.desktop',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+  });
+};
